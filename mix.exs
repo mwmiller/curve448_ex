ac99648155363c9451df8bb3bb41d014cebffbcb
Matt Miller <mwm@fastmail.com>
2019-01-13 08:41:00 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Curve448.Mixfile do
   def project do
     [
       app: :curve448,
-      version: "1.0.2",
-      elixir: "~> 1.4",
+      version: "1.0.3",
+      elixir: "~> 1.7",
       name: "Curve448",
       source_url: "https://github.com/mwmiller/curve448_ex",
       build_embedded: Mix.env() == :prod,
@@ -24,7 +24,7 @@ defmodule Curve448.Mixfile do
     [
       {:earmark, "~> 1.0", only: :dev},
       {:ex_doc, "~> 0.18", only: :dev},
-      {:credo, "~> 0.8", only: [:dev, :test]}
+      {:credo, "~> 1.0", only: [:dev, :test]}
     ]
   end
 
